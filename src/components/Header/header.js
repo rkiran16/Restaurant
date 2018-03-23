aa793d5ad0227979fbb87f49be7ff867139dc3e0
Ravi Kanculakunta <rkanculakunta@resourceammirati.com>
2018-03-22 21:01:26 -0400

    initial checkin

---
+++ src/components/Header/header.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Logo from './logo.png';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <div className="header-content">
+          <Link to="/">
+            <img className="logo" src={Logo} alt="Curry Bowl" />
+          </Link>
+          <div className="hamburger-container">
+            <input id="hamburger" type="checkbox" />
+            <label for="hamburger">
+              <div className="hamburger">
+                <span />
+                <span />
+                <span />
+              </div>
+            </label>
+            <nav>
+              <Link to="/" className="link">
+                About Us
+              </Link>
+              <Link to="/" className="link">
+                Menu
+              </Link>
+              <Link to="/" className="link">
+                Catering
+              </Link>
+              <Link to="/" className="link">
+                Gallery
+              </Link>
+              <Link to="/" className="link">
+                Hours
+              </Link>
+              <Link to="/" className="link">
+                Location
+              </Link>
+            </nav>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/footer/footer.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import Logo from './logo.png';
+import { Link } from 'react-router-dom';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <div className="location">
+          <a
+            target="_blank"
+            href="https://www.google.com/maps/place/4141+Hacks+Cross+Rd,+Memphis,+TN+38125/@35.03852,-89.795654,14z/data=!4m5!3m4!1s0x887f909f026b28a5:0x7a0da12ffd1f3f6c!8m2!3d35.0355972!4d-89.7966869?hl=en">
+            Curry Bowl on Google Maps{' '}
+          </a>
+        </div>
+        <div className="footer-content">
+          <div className="logo-container">
+            <Link to="/">
+              <img className="logo" src={Logo} alt="Curry Bowl" />
+            </Link>
+          </div>
+          <div className="bottom-link">
+            <h5>SITEMAP</h5>
+            <ul>
+              <li>
+                <Link className="link" to="/">
+                  HOME
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/about">
+                  ABOUT US
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/menu">
+                  MENU
+                </Link>
+              </li>
+              <li>
+                <Link className="link" to="/catering">
+                  CATERING
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="hours-container">
+            <h5>BUSINESS HOURS</h5>
+            <p className="timings">
+              Monday-Sunday <span>11AM TO 10.00PM</span>
+            </p>
+            <h6>Lunch Buffet</h6>
+            <p className="timings">
+              Monday-Thursday <span>11AM TO 2.20PM</span>
+            </p>
+            <p className="timings timings--weekend">
+              Saturday-Sunday <span>11:30AM TO 3PM</span>
+            </p>
+          </div>
+          <div className="contacts-container">
+            <h5>REACH US</h5>
+            <p>4141 Hacks Cross Road, Memphis, TN 38125</p>
+            <p>
+              CALL US:{' '}
+              <a className="link" href="tel://1-901-207-6051">
+                (901) 207-6051
+              </a>
+            </p>
+
+            <h6>FOLLOW US</h6>
+            <ul>
+              <li>FB</li>
+              <li>Instagram</li>
+              <li>Twitter</li>
+            </ul>
+          </div>
+        </div>
+        <div className="copyright">
+          <small>Curry Bowl © All Rights Reserved. 2017</small>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
